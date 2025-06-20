5b624d97b5c65252a51aff6cbd64ff37b91957e2
opubor <opubortony@gmail.com>
2025-06-20 23:11:06 +0100

    commit commit

--- components/ComponentX.tsx
+++ components/ComponentX.tsx
@@ -26,9 +26,9 @@ import { FaLocationDot } from "react-icons/fa6";
 import { MdLocalHospital } from "react-icons/md";
 
 const backgroundImages = [
-  { img: "/pic52.jpg" },
-  { img: "/pic48.jpg" },
-  { img: "/pic49.jpg" },
+  { img: "/pic16.jpg" },
+  { img: "/pic22.jpg" },
+  { img: "/pic23.jpg" },
 ];
 
 export default function BackgroundSwiper() {
@@ -36,11 +36,11 @@ export default function BackgroundSwiper() {
     <div className="relative w-full min-h-screen">
       {/* Swiper for background only */}
       <Swiper
-        modules={[EffectFade, Autoplay]}
-        effect="fade"
+        modules={[Autoplay]}
+        speed={800}
         autoplay={{ delay: 4000, disableOnInteraction: false }}
         loop={true}
-        className="mySwiper inset-0 w-full h-full z-0"
+        className="mySwiper absolute inset-0 w-full min-h-full z-0"
       >
         {backgroundImages.map((data, i) => (
           <SwiperSlide key={i}>
@@ -53,10 +53,105 @@ export default function BackgroundSwiper() {
       </Swiper>
 
       {/* Optional Overlay */}
-      <div className="absolute inset-0 bg-black/50 z-10" />
+      <div className="absolute inset-0 bg-black/70 z-10" />
 
       {/* Foreground Content */}
-      <div className="absolute inset-0 z-20 flex items-center justify-center text-white">
+      <div className="absolute inset-0 z-20 flex items-center justify-center text-white min-h-screen">
+        <div className="bg-center bg-cover  w-full flex flex-col justify-start items-start pt-80 lg:pt-72 pb-8 lg:pb-0">
+          <div
+            className="px-2 lg:px-12 xl:px-36 flex flex-col justify-start items-start w-full"
+            data-aos="fade-left"
+          >
+            <h1 className="text-white p-2 lg:p-4 text-2xl lg:text-5xl font-extrabold w-full tracking-tighter flex justify-start items-center gap-2">
+              Your Health, Our Priority <FaCheckDouble />
+            </h1>
+            <p className="text-sm lg:text-lg my-4 lg:my-6 text-white p-2 lg:p-4 w-full lg:w-8/12">
+              We’re dedicated to providing exceptional, patient-centered
+              healthcare using the latest medical innovations — with compassion,
+              integrity, and a deep commitment to your total well-being.
+            </p>
+            {/* <p className="text-base lg:text-lg my-4 lg:my-8 text-white p-2 lg:p-4 w-full">
+              Expert care, modern technology, and a compassionate team — all in
+              one place.
+            </p> */}
+
+            <div className="w-full flex flex-col lg:flex-row justify-start items-start gap-4 lg:gap-8">
+              <Link
+                href={"/#our_services"}
+                className="border-2 border-white bg-darkblue text-white py-3 px-6 text-sm hover:bg-blue rounded-full flex justify-center items-center gap-2"
+              >
+                <MdLocalHospital className="text-lg" />
+                Our Services
+              </Link>
+              <Link
+                href={"/#book_appointment"}
+                className="border-2 border-white bg-white text-darkblue py-3 px-6 text-sm hover:bg-blue rounded-full flex justify-center items-center gap-2"
+              >
+                <FaRegCalendarAlt className="text-lg" />
+                Book an Appointment
+              </Link>
+            </div>
+
+            <div className="w-full flex justify-start">
+              <div className="text-darkblue mt-8 bg-white p-6 rounded-2xl">
+                <h1 className="underline underline-offset-2 uppercase mb-2">
+                  Contact us
+                </h1>
+                <p className="flex justify-start items-center gap-2 text-sm">
+                  <FaLocationDot /> {contactInformation?.at(0)?.address}
+                </p>
+                <Link
+                  href={`https://wa.me/${contactInformation?.at(0)?.whatsapp}`}
+                  target="_blank"
+                  className="flex justify-start items-center gap-2 text-sm mt-2"
+                >
+                  <IoLogoWhatsapp /> {contactInformation?.at(0)?.whatsapp}
+                </Link>
+                <Link
+                  href={`tel:${contactInformation?.at(0)?.phone1!}`}
+                  target="blank"
+                  className="flex justify-start items-center gap-2 text-sm mt-2"
+                >
+                  <IoCall /> {contactInformation?.at(0)?.phone1}
+                </Link>
+                <Link
+                  href={`tel:${contactInformation?.at(0)?.phone2!}`}
+                  target="blank"
+                  className="flex justify-start items-center gap-2 text-sm mt-2"
+                >
+                  <IoCall /> {contactInformation?.at(0)?.phone2}
+                </Link>
+                <p className="flex justify-start items-center gap-2 text-sm mt-2">
+                  <FaClock /> {contactInformation?.at(0)?.openingHours}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between w-full">
+            <div className="hidden lg:flex w-5/12"></div>
+            <div className="w-full lg:w-7/12 backdrop-blur-md flex justify-between items-center mt-16 lg:border-l lg:border-l-white border-t border-t-white lg:rounded-tl-3xl">
+              <div className="text-white flex flex-col justify-center items-center gap-1 p-8 w-3/12">
+                <FaShieldAlt className="text-2xl lg:text-6xl" />
+                <h1 className="text-xs lg:text-sm">Trusted</h1>
+              </div>
+              <div className="text-white flex flex-col justify-center items-center gap-1 p-8 w-3/12">
+                <HiBadgeCheck className="text-2xl lg:text-6xl" />
+                <h1 className="text-xs lg:text-sm">Certified</h1>
+              </div>
+              <div className="text-white flex flex-col justify-center items-center gap-1 p-8 w-3/12">
+                <FaMedal className="text-2xl lg:text-6xl" />
+                <h1 className="text-xs lg:text-sm">Accredited</h1>
+              </div>
+              <div className="text-white flex flex-col justify-center items-center gap-1 p-8 w-3/12">
+                <FaUserMd className="text-2xl lg:text-6xl" />
+                <h1 className="text-xs lg:text-sm">Experienced</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="absolute inset-0 z-20 flex items-center justify-center text-white">
         <div className="bg-center bg-cover lg:min-h-screen w-full flex flex-col justify-start items-start pt-72 pb-8 lg:pb-0">
           <div
             className="px-2 lg:px-12 xl:px-36 flex flex-col justify-start items-start w-full"
@@ -143,7 +238,7 @@ export default function BackgroundSwiper() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
